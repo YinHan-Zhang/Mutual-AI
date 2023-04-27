997c717bd2ca046361f013dbf0069fa69c6d66cd
moyitech <admin@9998k.cn>
2023-04-27 20:22:23 +0800

    修改import

--- routers/written_digit_recognition.py
+++ routers/written_digit_recognition.py
@@ -3,7 +3,6 @@
 # @Author:MoyiTech
 # @Software: PyCharm
 from fastapi import APIRouter, FastAPI, File, Form, UploadFile
-from fastapi.responses import JSONResponse
 import model.written_digit_recognition.main
 
 router = APIRouter()
@@ -11,7 +10,6 @@ router = APIRouter()
 
 @router.post("/written_digit_recognition")
 async def written_digit_recognition(file: UploadFile):
-    print(file.content_type)
     res, used_time = model.written_digit_recognition.main.recognize(file.file)
     return {
         "filename": file.filename,
