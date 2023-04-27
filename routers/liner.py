489c80b6e012b86fdaae2eda7d7f738af6da52ff
sizhouLiu <lsz8824523@163.com>
2023-04-27 13:23:25 +0800

    变更

--- routers/liner.py
+++ routers/liner.py
@@ -5,8 +5,9 @@ from model import Liner
 router = APIRouter()
 liner_model = Liner.Liner_Model()
 
-@router.get("/liner/{data}")
+@router.get("/liner")
 async def liner(data: int):
+    data: int
     return JSONResponse({
         "code": 200,
         "data": {
