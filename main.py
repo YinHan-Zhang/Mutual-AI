ddcdfebef94775f81fd4990c74b9c0de6639016d
ErickRen <ErickRen2022@outlook.com>
2023-04-29 22:51:29 +0800

    feat: 添加中间件。

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from routers import liner, written_digit_recognition, poetry_generator
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -12,6 +13,18 @@ app.include_router(poetry_generator.router)
 app.docs_url = None
 app.redoc_url = None
 
+origins = [
+    "http://api.9998k.cn",
+    "https://api.9998k.cn:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def main():
