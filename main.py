4e38f5df854c0be22cdb77ca89f6e60be5a5aea2
sizhouLiu <lsz8824523@163.com>
2023-05-05 10:39:37 +0800

    写了注释

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 app.docs_url = None
 app.redoc_url = None
 
+# 跨域
 origins = [
     "http://ai.9998k.cn",
     '127.0.0.1',
@@ -23,6 +24,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(liner.router)
 app.include_router(written_digit_recognition.router)
