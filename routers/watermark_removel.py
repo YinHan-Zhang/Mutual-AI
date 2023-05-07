04f455877bb7d6f66f0e79848f8119493f170215
sizhouLiu <lsz8824523@163.com>
2023-05-07 19:36:27 +0800

    增加了去除水印的接口 以及模型(但是没弄好）

--- routers/watermark_removel.py
+++ routers/watermark_removel.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 # import model.watermark_removal.main
+
+# 由于tensorflow 使用的1.14版本 与main函数其他模型不兼容先不导进来
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 
