cba827e792517900cf8bbf59d7929e029af901f7
sizhouLiu <lsz8824523@163.com>
2023-05-08 21:54:45 +0800

    增加了去除水印的接口以及接口文档理论上完成了 需要试验一下

--- routers/watermark_removel.py
+++ routers/watermark_removel.py
@@ -1,23 +1,24 @@
-from fastapi import APIRouter
-import model.watermark_removal.main
-
-# 由于tensorflow 使用的1.14版本 与main函数其他模型不兼容先不导进来
-from pydantic import BaseModel
-from fastapi.responses import JSONResponse
-import cv2
-from starlette.responses import StreamingResponse
-from io import BytesIO
-import base64
-router = APIRouter()
-
-
-class Item(BaseModel):
-    img: str
-
-
+# from fastapi import APIRouter
+# import model.watermark_removal.main
+#
+# # 由于tensorflow 使用的1.14版本 与main函数其他模型不兼容先不导进来
+# from pydantic import BaseModel
+# from fastapi.responses import JSONResponse
+# import cv2
+# from starlette.responses import StreamingResponse
+# from io import BytesIO
+# import base64
+# router = APIRouter()
+#
+#
+# class Item(BaseModel):
+#     img: str
+#
+#
 # @router.post("/watermark_removal")
 # async def watermark_removal(item: Item):
-#     result = model.watermark_removal.main.recognize(item.img)
+#     print(1)
+#     result = model.watermark_removal.main.watermark_removel(item.img)
 #     img = cv2.imdecode(result, cv2.IMREAD_COLOR)
 #     result_2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #     img_bytes = cv2.imencode('.jpg', result_2)[1].tobytes()
